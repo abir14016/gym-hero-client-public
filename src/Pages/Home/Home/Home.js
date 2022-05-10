9d2b608e21b29c522c3b526ca2644c922325bb8a
abir14016 <abir14016@gmail.com>
2022-05-10 20:49:10 +0600

    image slider and dynamic title

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from '../../Shared/PageTitle/PageTitle';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <PageTitle title="Home"></PageTitle>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </HelmetProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
