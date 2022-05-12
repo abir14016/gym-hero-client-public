e62412b8e2a8916699c71d6a59fdf4078d2373ed
abir14016 <abir14016@gmail.com>
2022-05-12 19:10:43 +0600

    Restock button implemented

--- src/Pages/InventoryDetail/InventoryDetail.js
+++ src/Pages/InventoryDetail/InventoryDetail.js
@@ -11,19 +11,19 @@ const InventoryDetail = () => {
     const [inventory, setInventory] = UseInventoryDetail(inventoryId)
     const { _id, name, picture, deccription, price, quantity, supplier } = inventory;
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
 
     const handleDelivered = () => {
-        const { quantity, _id, ...rest } = inventory;
+        const { quantity, ...rest } = inventory;
         const updatedInventory = {
-            quantity: quantity - 1, ...rest
+            quantity: parseInt(quantity) - 1, ...rest,
         }
         if (updatedInventory.quantity < 0) {
             return toast.error("Stock Out");
         }
         else {
-            const proced = window.confirm(`Please confirm to deliver one ${name} `);
+            const proced = window.confirm(`Please confirm to deliver 1 ${name} `);
             if (proced) {
                 (async function () {
                     console.log(updatedInventory);
@@ -37,6 +37,37 @@ const InventoryDetail = () => {
         }
     }
 
+    const handleRestock = event => {
+        event.preventDefault();
+        const restockNumber = parseInt(event.target.restock.value);
+        const { quantity, ...rest } = inventory;
+        const updatedInventory = {
+            quantity: parseInt(quantity) + restockNumber, ...rest,
+        }
+        const proced = window.confirm("Do you want to restock this inventory");
+        if (proced) {
+            if (!restockNumber >= 1) {
+                return toast.error("Enter a valid number");
+            }
+            const url = `http://localhost:5000/inventory/${_id}`;
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(updatedInventory)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(updatedInventory);
+                    console.log('success', data);
+                    setInventory(updatedInventory);
+                    toast.success("Restock Successfull");
+                    event.target.reset();
+                })
+        }
+    }
+
     return (
         <div>
             <h2 className='text-center'>Detailed Information For: {name}</h2>
@@ -45,6 +76,10 @@ const InventoryDetail = () => {
                     <div className="col-md-4 text-center">
                         <img src={picture} className="img-fluid rounded-start" alt="" />
                         <h6 className='text-center mt-2'>ID: {_id}</h6>
+                        <form onSubmit={handleRestock} className='restock-form d-flex justify-content-around'>
+                            <input className="w-50 text-center fw-bolder" type="number" name="restock" min={1} max={Infinity} defaultValue={1} placeholder='Add Quantity' id="restock" />
+                            <input type="submit" value="Restock" className='btn btn-primary'></input>
+                        </form>
                     </div>
                     <div className="col-md-8">
                         <div className="card-body text-center border">
@@ -58,7 +93,7 @@ const InventoryDetail = () => {
                 </div>
             </div>
             <div className='text-center'>
-                <h2>Detail Information:</h2>
+                <h2>Description:</h2>
                 <p>{deccription}</p>
             </div>
         </div>
